86cdb4f89e02ca6533722b17d53d8b9ea0be3a44
ykbhrn <jakub.horun@gmail.com>
2020-06-17 22:20:31 +0100

    chat progress

--- frontend/src/components/common/Chat.js
+++ frontend/src/components/common/Chat.js
@@ -5,7 +5,8 @@ import { Link } from 'react-router-dom'
 
 class Chat extends React.Component {
   state = {
-    user: null
+    user: null,
+    showMessages: false
   }
 
   async componentDidMount() {
@@ -17,26 +18,78 @@ class Chat extends React.Component {
     }
   }
 
+  uniq(a) {
+    var seen = {}
+    return a.filter(function (item) {
+      return seen.hasOwnProperty(item) ? false : (seen[item] = true)
+    })
+  }
+
 
 
   render() {
     if (!this.state.user) return null
-    console.log(this.state)
+    // console.log(this.state)
     return (
-      <div>
-        {/* Find Users who i have chat history with */}
-        {this.state.user.chats.map( chat => {
-          const notCurrentUser = chat.users.filter(user => {
-            return user.id !== this.state.user.id
-          })
-          return notCurrentUser.map( userForChat => (
-            <div key={userForChat.id} className="profile-header-index">        
-              <Link to={`/profile/${userForChat.id}`}>
-                <img className='profile-image-index' src={userForChat.profile_image}/></Link>
-              <Link to={`/profile/${userForChat.id}`}>{userForChat.username}</Link>
+      <div className="chat-profile-container">
+        <div className="chat-profile-side">
+          {/* Find Users who i have chat history with */}
+          {this.state.user.chats.slice(0).reverse().map(chat => {
+            const notCurrentUser = chat.users.filter(user => {
+              return user.id !== this.state.user.id
+            })
+            // const uniq = [ ...new Set(notCurrentUser) ]
+            // notCurrentUser.filter((item, index) => notCurrentUser.indexOf(item.id) === index )
+            // notCurrentUser.reduce((unique, item) => 
+            //   unique.includes(item.id) ? unique : [...unique, item.id], [])
+            const uniqueArray = notCurrentUser.filter((thing, index) => {
+              const _thing = JSON.stringify(thing)
+              return index === notCurrentUser.findIndex(obj => {
+                return JSON.stringify(obj) === _thing
+              })
+            })
+            return notCurrentUser.map(userForChat => (
+              <div key={userForChat.id} className="profile-header-chat-page">
+                <Link to={`/profile/${userForChat.id}`}>
+                  <img className='profile-image-index' src={userForChat.profile_image} /></Link>
+                <Link to={`/profile/${userForChat.id}`}>{userForChat.username}</Link>
+              </div>
+            ))
+          })}
+        </div>
+
+        {/* Chat history with specific user and sending message form*/}
+        <div className="messages-section">
+
+          <div className="profile-header profile-messages">
+            <img className='profile-image' src={this.state.user.profile_image} />
+            <div className="greeting-public"><span className='title is-2'>{this.state.user.username}</span>
+              <div className="user-type"><img src={`${this.state.isStudent ? '/images/student.png' : '/images/athlete.png'}`} />
+                {this.state.user.user_type.name} 
+              </div>
             </div>
-          ))
-        })}
+          </div>
+          
+          <div className="chat-history-container">
+            <div className="chat-history">
+
+            </div>
+          </div>
+
+          <div className="messages-form-container">
+            <form onSubmit={(event) => {
+              this.handleSubmit(event,)
+            }}>
+              <input className="input" 
+                name='text'
+                onChange={this.handleChange}
+              />
+              <button className="comment-button">Send</button>
+            </form>
+          </div>       
+
+        </div>
+
       </div>
     )
   }
